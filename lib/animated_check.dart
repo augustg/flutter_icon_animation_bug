981887bce10a849e50d486c9e9cfb3a71f6ead7b
August Gustavsson <august@kattalo.com>
2020-10-07 17:07:48 +0200

    Initial commit

---
+++ lib/animated_check.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class AnimatedCheck extends StatefulWidget {
+  final bool animate;
+  final double size;
+
+  AnimatedCheck({
+    Key key,
+    @required this.animate,
+    this.size = 72.0,
+  }) : super(key: key);
+
+  @override
+  _AnimatedCheckState createState() => _AnimatedCheckState();
+}
+
+class _AnimatedCheckState extends State<AnimatedCheck>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _scaleAnimation;
+  bool _isAnimating = false;
+
+  @override
+  void didUpdateWidget(AnimatedCheck oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.animate != widget.animate) {
+      print(widget.animate);
+      if (_isAnimating && !widget.animate) {
+        _stop();
+      }
+      if (!_isAnimating && widget.animate) {
+        _start();
+        print(widget.animate);
+      }
+    }
+  }
+
+  void _start() {
+    _animationController.forward();
+
+    _isAnimating = true;
+  }
+
+  void _stop() {
+    _animationController.stop();
+    _animationController.reset();
+    _isAnimating = false;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 700),
+      vsync: this,
+      value: 0,
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.elasticInOut,
+      ),
+    );
+
+    if (widget.animate) {
+      _start();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      // child: Container(
+      //   width: widget.size,
+      //   height: widget.size,
+      //   color: Colors.green,
+      // )
+      child: Icon(
+        Icons.check,
+        color: Colors.green,
+        size: widget.size,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+
+    super.dispose();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:icon_animation_bug/animated_check.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _animate = false;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+
+      _animate = !_animate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedCheck(animate: _animate),
+            MaterialButton(
+                child: Text(
+                    _animate ? 'Press to reset animation' : 'Press to animate'),
+                onPressed: _incrementCounter),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
